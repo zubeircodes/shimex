171e3fcdaf062247f08efee1477238ee72347601
Zubeir <zubeir06@yahoo.com>
2023-02-15 19:54:31 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Landing from './components/Landing';
 import Features from './components/Features';
 import Contact from './components/Contact';
 
+// test commit
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
